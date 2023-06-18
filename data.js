d83a02e7cedf1703a0b09da789014a5f9f82fca2
ayangjibrut <129623348+ayangjibrut@users.noreply.github.com>
2023-06-19 00:06:33 +0700

    Update

--- data.js
+++ data.js
@@ -1,23 +1,37 @@
-const menuArray = [
+export const menuArray = [
     {
-        name: "Pizza",
+        name: "Cotton Candy Pizza",
         ingredients: ["pepperoni", "mushrom", "mozarella"],
-        id: 0,
         price: 14,
-        emoji: "🍕"
+        emoji: "🍕",
+        id: 0
     },
-    {
-        name: "Hamburger",
+        {
+        name: "Jalapeno Spicy Pizza",
         ingredients: ["beef", "cheese", "lettuce"],
         price: 12,
-        emoji: "🍔",
+        emoji: "🍕",
         id: 1
+    },
+        {
+        name: "Twilight Pizza",
+        ingredients: ["beef", "cheese", "lettuce"],
+        price: 11,
+        emoji: "🍕",
+        id: 2
     },
         {
         name: "Beer",
         ingredients: ["grain, hops, yeast, water"],
-        price: 12,
+        price: 7,
         emoji: "🍺",
-        id: 2
+        id: 3
+    },
+        {
+        name: "Tropical Juice",
+        ingredients: ["pineapple, orange, watermelon, rambutan"],
+        price: 9,
+        emoji: "🥤",
+        id: 4
     }
 ]
---
+++ index.js
@@ -0,0 +1,15 @@
+import { menuArray } from '/data.js'
+
+// function getMenuHtml() {
+//     let menuHtml = ''
+
+//     menuHtml += `
+//         <div></div>
+//     `
+// }
+
+// function render() {
+//     document.getElementById('menus').innerHTML = getMenuHtml()
+// }
+
+// render()
