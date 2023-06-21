240e3cc92cb6ae5827ade56a21ca54f504c55895
ayangjibrut <129623348+ayangjibrut@users.noreply.github.com>
2023-06-21 12:43:02 +0700

    update

--- data.js
+++ data.js
@@ -1,37 +1,44 @@
+import { v4 as uuidv4 } from 'https://jspm.dev/uuid';
+
 export const menuArray = [
     {
-        name: "Cotton Candy Pizza",
-        ingredients: ["pepperoni", "mushrom", "mozarella"],
-        price: 14,
-        emoji: "🍕",
-        id: 0
+        name: `Cotton Candy Pizza`,
+        ingredients: "the flavors of fairy floss and pizza",
+        picture: 'images/cotton-candy.jpg',
+        price: 14.3,
+        id: 0,
+        uuid: uuidv4()
     },
         {
         name: "Jalapeno Spicy Pizza",
-        ingredients: ["beef", "cheese", "lettuce"],
-        price: 12,
-        emoji: "🍕",
-        id: 1
+        ingredients: "great choice for those who enjoy a little heat in their food",
+        picture: 'images/jalapeno.png',
+        price: 12.1,
+        id: 1,
+        uuid: uuidv4()
     },
         {
         name: "Twilight Pizza",
-        ingredients: ["beef", "cheese", "lettuce"],
-        price: 11,
-        emoji: "🍕",
-        id: 2
+        ingredients: "doesn't refer to a specific pizza recipe or a well-known pizza variety",
+        picture: 'images/twilight.png',
+        price: 11.4,
+        id: 2,
+        uuid: uuidv4()
     },
         {
         name: "Beer",
-        ingredients: ["grain, hops, yeast, water"],
-        price: 7,
-        emoji: "🍺",
-        id: 3
+        ingredients: "grain, hops, yeast, water",
+        picture: 'images/beer.jpg',
+        price: 7.2,
+        id: 3,
+        uuid: uuidv4()
     },
         {
         name: "Tropical Juice",
-        ingredients: ["pineapple, orange, watermelon, rambutan"],
-        price: 9,
-        emoji: "🥤",
-        id: 4
+        ingredients: "mango, orange, rambutan, avocado",
+        picture: 'images/tropical-juice.jpg',
+        price: 9.4,
+        id: 4,
+        uuid: uuidv4()
     }
 ]
--- index.js
+++ index.js
@@ -1,15 +1,69 @@
-import { menuArray } from '/data.js'
+import { menuArray } from './data.js'
 
-// function getMenuHtml() {
-//     let menuHtml = ''
+const basket = []
 
-//     menuHtml += `
-//         <div></div>
-//     `
-// }
+document.addEventListener('click', function(e) {
+    if (e.target.dataset.addmenu) {
+        handleAddClick(e.target.dataset.addmenu)
+    }
+})
 
-// function render() {
-//     document.getElementById('menus').innerHTML = getMenuHtml()
-// }
+function handleAddClick(menuId) {
+    const targetMenuObj = menuArray.filter(function(menu) {
+        return menu.id === menuId
+    })
+    menuArray.unshift(targetMenuObj)
+    render()
+}
 
-// render()
+function getMenuHtml() {
+    let menuItem = ``
+    let menuHtml = ``
+
+    menuArray.forEach(function(menu) {
+
+        menuItem += `
+        <div class="menu">
+            <img src="${menu.picture}" class="menu-img">
+            <ul>
+                <li class="bold">${menu.name}</li>
+                <li>${menu.ingredients}</li>
+            </ul>
+            <div class="item-wrapper">
+                <h3>$${menu.price}</h3>
+                <button><i class="fa-solid fa-plus fa-xl" data-addmenu="${menu.id}"></i></button>
+            </div>
+        </div>
+    `
+    })
+    menuHtml += menuItem
+    return menuHtml
+}
+
+function createAside() {
+    document.getElementById('cart').innerHTML = createAside()
+    let asideHtml = `
+        <h4>Your Order</h4>
+        <div class="shopping-cart" id="shopping-cart">
+            <h3>${menu.name}</h3>
+        </div>
+    `
+    asideHtml += ``
+    return asideHtml
+}
+
+function render() {
+    document.getElementById('menus').innerHTML = getMenuHtml()
+
+    const aside = document.querySelector('aside')
+
+    aside.innerHTML = createAside()
+        if (menuArray.length === 0) {
+            aside.classList.add(`hidden`)
+        }
+        else {
+            aside.classList.remove(`hidden`)
+        }
+}
+
+render()
