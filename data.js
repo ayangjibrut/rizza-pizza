63a38689638b8be9a17385cbe2fcb6d96e974d13
ayangjibrut <129623348+ayangjibrut@users.noreply.github.com>
2023-06-22 19:05:55 +0700

    update

--- data.js
+++ data.js
@@ -1,44 +1,48 @@
-import { v4 as uuidv4 } from 'https://jspm.dev/uuid';
-
+import { v4 as uuidv4 } from 'https://jspm.dev/uuid'
 export const menuArray = [
     {
         name: `Cotton Candy Pizza`,
         ingredients: "the flavors of fairy floss and pizza",
         picture: 'images/cotton-candy.jpg',
-        price: 14.3,
+        price: 14,
         id: 0,
+        numberOrdered: 0,
         uuid: uuidv4()
     },
         {
         name: "Jalapeno Spicy Pizza",
         ingredients: "great choice for those who enjoy a little heat in their food",
         picture: 'images/jalapeno.png',
-        price: 12.1,
+        price: 12,
         id: 1,
+        numberOrdered: 0,
         uuid: uuidv4()
     },
         {
         name: "Twilight Pizza",
         ingredients: "doesn't refer to a specific pizza recipe or a well-known pizza variety",
         picture: 'images/twilight.png',
-        price: 11.4,
+        price: 11,
         id: 2,
+        numberOrdered: 0,
         uuid: uuidv4()
     },
         {
-        name: "Beer",
+        name: "Nebula Beer",
         ingredients: "grain, hops, yeast, water",
-        picture: 'images/beer.jpg',
-        price: 7.2,
+        picture: 'images/nebula-beer.jpg',
+        price: 10,
         id: 3,
+        numberOrdered: 0,
         uuid: uuidv4()
     },
         {
-        name: "Tropical Juice",
+        name: "Underwater Tropical Juice",
         ingredients: "mango, orange, rambutan, avocado",
-        picture: 'images/tropical-juice.jpg',
-        price: 9.4,
+        picture: 'images/underwater-tropical-juice.jpg',
+        price: 11,
         id: 4,
+        numberOrdered: 0,
         uuid: uuidv4()
     }
 ]
--- index.js
+++ index.js
@@ -1,27 +1,11 @@
 import { menuArray } from './data.js'
 
-const basket = []
-
-document.addEventListener('click', function(e) {
-    if (e.target.dataset.addmenu) {
-        handleAddClick(e.target.dataset.addmenu)
-    }
-})
-
-function handleAddClick(menuId) {
-    const targetMenuObj = menuArray.filter(function(menu) {
-        return menu.id === menuId
-    })
-    menuArray.unshift(targetMenuObj)
-    render()
-}
-
-function getMenuHtml() {
-    let menuItem = ``
+// Render restaurant menu
+function getFeedHtml() {
+    let feedHtml = ``
 
     menuArray.forEach(function(menu) {
-
-        menuItem += `
+        feedHtml += `
         <div class="menu">
             <img src="${menu.picture}" class="menu-img">
             <ul>
@@ -30,16 +14,192 @@ function getMenuHtml() {
             </ul>
             <div class="item-wrapper">
                 <h3>$${menu.price}</h3>
-                <button><i class="fa-solid fa-plus fa-xl" data-addmenu="${menu.id}"></i></button>
+                <button class="add-button"><i class="fa-solid fa-plus fa-xl" data-add-btn="${menu.uuid}"></i></button>
             </div>
         </div>
     `
     })
-    return menuItem
+    return feedHtml
 }
 
 function render() {
-    document.getElementById('menus').innerHTML = getMenuHtml()
+    document.getElementById('section-el').innerHTML = getFeedHtml()
 }
 
 render()
+
+// Handle button, display ordering process in html
+const orderHtml = document.getElementById('order-process')
+const paymentModal = document.querySelector('form')
+const overlay = document.querySelector('.overlay')
+const orderTitleHtml = document.querySelector('.title')
+const summaryEl = document.querySelector('#summary')
+const emptyOrder = document.querySelector('.empty-order')
+
+document.addEventListener('click', function(e) {
+    if (e.target.dataset.addBtn) {
+        handleAddBtnClick(e.target.dataset.addBtn)
+    }
+    else if (e.target.dataset.removeBtn) {
+        handleRemoveBtnClick(e.target.dataset.removeBtn)
+    }
+    else if (e.target.dataset.completeOrder) {
+        handleCompleteOrderBtnClick(e.target.dataset.completeOrder)
+    } 
+    else if (e.target.dataset.closeModal) {
+        handleCloseModalBtnClick(e.target.dataset.closeModal)
+    }
+})
+
+document.addEventListener('submit', function(e) {
+    if (e.target.dataset.pay) {
+        return e.target.dataset.pay
+    }
+})
+
+const orderDisplay = () => {
+    orderTitleHtml.classList.remove('hidden')
+    emptyOrder.classList.add('hidden')
+}
+
+const orderProcess = function() {
+    orderHtml.innerHTML = ``
+    summaryEl.innerHTML = ''
+
+    let totalPrice = 0
+
+    orderArray.forEach(function([name, numberOrdered, price, uuid]) {
+        orderHtml.innerHTML += `
+            <div class="shopping-cart" id="shopping-cart">
+                <div class="menu-order">
+                    <p class="menu-name" id="menu-name">${name}
+                    <span class="remove-btn" id="remove-btn" data-remove=${uuid} data-remove-btn=${uuid}>remove</span></p>
+                </div>
+                <div class="price-order" id="price-order">
+                    <p class="order-price"><small>${numberOrdered}x</small><h3>$${price}</h3>
+                </div>
+            </div>
+        `
+        totalPrice += price
+    })
+
+    summaryEl.innerHTML = `
+        <div class="summary">
+            <h3 class="total-price">Total price:</h3>
+            <h3 id="total-price">$${totalPrice}</h3>
+        </div>
+        <div class="footer">
+            <button class="checkout" id="checkout" data-complete-order="complete-order">Complete Order</button>
+        </div>
+    `
+
+    if (totalPrice === 0) {
+        orderTitleHtml.classList.add('hidden')
+        orderHtml.innerHTML = ``
+        summaryEl.innerHTML = ''
+    }
+}
+
+// Add remove buttons
+let orderArray = []
+
+function handleAddBtnClick(addBtnId) {
+    const targetAddBtn = menuArray.filter(function(menuItem) {
+        return menuItem.uuid === addBtnId
+    })[0]
+
+    targetAddBtn.numberOrdered++
+    if (targetAddBtn.numberOrdered === 1) {
+        orderArray.push([
+            targetAddBtn.name,
+            targetAddBtn.numberOrdered,
+            targetAddBtn.price,
+            targetAddBtn.uuid
+        ])
+    }
+    else {
+        for (let i = 0; i < orderArray.length; i++) {
+            if (orderArray[i][0] === targetAddBtn.name) {
+                orderArray[i][1] = targetAddBtn.numberOrdered
+                orderArray[i][2] = targetAddBtn.price *targetAddBtn.numberOrdered
+                orderArray[i][3] = targetAddBtn.uuid
+            }
+        }
+    }
+    orderDisplay()
+    orderProcess()
+}
+
+function handleRemoveBtnClick(removeBtnId) {
+    const targetRemoveBtn = menuArray.filter(function(menuItem) {
+        return menuItem.uuid === removeBtnId
+    })[0]
+
+    targetRemoveBtn.numberOrdered--
+    for (let i=0; i < orderArray.length; i++) {
+        if (orderArray[i][0] === targetRemoveBtn.name) {
+            orderArray[i][1] = targetRemoveBtn.numberOrdered
+            orderArray[i][2] = targetRemoveBtn.price * targetRemoveBtn.numberOrdered
+        }
+        if (orderArray[i][1] === 0) {
+            orderArray.splice(i, 1)
+        }
+    }
+    orderProcess()
+}
+
+// Complete order btn
+function handleCompleteOrderBtnClick() {
+    openModal()
+}
+
+// Close payment modal
+function handleCloseModalBtnClick() {
+    closeModal()
+}
+
+document.addEventListener('keydown', function(e) {
+    if (e.key === 'Escape' && !paymentModal.classList.contains('hidden')) {
+        closeModal()
+    }
+})
+
+const openModal = function() {
+    paymentModal.classList.remove('hidden')
+    overlay.classList.remove('hidden')
+}
+
+const closeModal = function() {
+    paymentModal.classList.add('hidden')
+    overlay.classList.add('hidden')
+    event.preventDefault()
+}
+
+// Pay btn
+paymentModal.onsubmit = function() {
+    closeModal()
+
+    const buyerName = document.getElementById('buyer-name').value
+
+    orderTitleHtml.classList.add('hidden')
+
+    orderHtml.innerHTML = ''
+    summaryEl.innerHTML = ''
+    orderHtml.innerHTML = `
+        <div class="thank-you-message">
+            <h3>Thanks! ${buyerName}! Your order is on it's way!</h3>
+        </div>
+        `
+
+    menuArray.forEach(function(menu) {
+        menu.numberOrdered = 0
+    })
+    event.preventDefault()
+    clearAllInputs()
+}
+
+// Clear input fields
+function clearAllInputs(event) {
+    let allInputs = document.querySelectorAll('input')
+    allInputs.forEach(singleInput => (singleInput.value = ''))
+}
